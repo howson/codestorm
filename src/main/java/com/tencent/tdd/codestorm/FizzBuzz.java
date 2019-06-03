5874aa2e38e028446a9772da492e4f44933a4be1
howsonliu(刘浩深) <howsonliu@tencent.com>
2019-06-03 14:40:42 +0800

    先把大概有哪些条件，以及游戏应该怎么进行初步编排

--- src/main/java/com/tencent/tdd/codestorm/FizzBuzz.java
+++ src/main/java/com/tencent/tdd/codestorm/FizzBuzz.java
@@ -1,5 +1,56 @@
 package com.tencent.tdd.codestorm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FizzBuzz {
+
+  public Map<Integer, String> fizzbuzzMap = new HashMap<>();
+
+  public int tellTwoSpecialNum(int x, int y) {
+    return fizzbuzzMap.size();
+  }
+
+  public boolean isFizz(int number) {
+    return false;
+  }
+
+  public boolean isBuzz(int number) {
+    return false;
+  }
+
+  public boolean isFizzBuzz(int number) {
+    return false;
+  }
+
+  public void sayYourSlogan(int number) {
+
+    if (isFizzBuzz(number)) {
+      System.out.println("FizzBuzz");
+      return;
+    }
+
+    if (isFizz(number)) {
+      System.out.println("Fizz");
+      return;
+    }
+
+    if (isBuzz(number)) {
+      System.out.println("Buzz");
+      return;
+    }
+
+    System.out.println(number);
+    return;
+  }
+
+  public void startGame(int studentNumber, int x, int y) {
+    tellTwoSpecialNum(x, y);
+
+    for (int i = 0; i < studentNumber; i++) {
+      sayYourSlogan(i);
+    }
+
+  }
 
 }
---
+++ src/test/java/com/tencent/tdd/codestorm/FizzBuzzTest.java
@@ -0,0 +1,40 @@
+package com.tencent.tdd.codestorm;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+  // 输入两个特殊数x和y
+  // 遇到包含x或是x的倍数，输出Fizz
+  // 遇到包含y或是y的倍数，输出Buzz
+  // 同时是x和y的倍数，喊FizzBuzz
+  // 同时满足上述多个条件，Fizz和Bizz只能出现一次
+  // 否则输出数字
+
+  private FizzBuzz fizzBuzz = new FizzBuzz();
+
+  /**
+   * 测试Fizz是否正常
+   */
+  @Test
+  public void testFizz() {
+
+  }
+
+  /**
+   * 测试Buzz是否正常
+   */
+  @Test
+  public void testBuzz() {
+
+  }
+
+  /**
+   * 测试FizzBuzz是否正常
+   */
+  @Test
+  public void testFizzBuzz() {
+
+  }
+
+}
